12f1dd85e012a01841ea4802131d0c56a28075ca
Marius <marius-sol@pm.me>
2022-08-28 16:02:54 +0200

    Starting rewriting in Golang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xaner4/dotsync
+
+go 1.18
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -65,19 +66,65 @@ func askProfileInstall(profiles []string) []string {
 
 func install(path string) (bool, error) {
 	files, err := ioutil.ReadDir(path)
+	// var input string
 	if err != nil {
 		return false, err
 	}
 	for _, f := range files {
+		// rp = realpath
+		var rp string
+		src := filepath.Join(path, f.Name())
+		dst := filepath.Join(INSTALLDIR, f.Name())
+		s, err := os.Stat(dst)
+		Exists := !errors.Is(err, os.ErrNotExist)
+		if err != nil {
+			fmt.Println(err)
+		}
 
+		if Exists {
+			rp, _ = os.Readlink(dst)
+		}
+
+		if src == rp {
+			fmt.Printf("%s is already installed to %s \n", src, dst)
+			// TODO: Check if src is folder, if folder, check if ther exists any files inside and call install on them! (WARNING: Recursion may happen)
+			continue
+		}
+
+		if dst != rp {
+			fmt.Printf("s: %s\n\n", s)
+			fmt.Printf("%s is a symlink to %s \n", dst, rp)
+
+			// fmt.Printf("Do you want to overwrite link %s [y/N]: \n", dst)
+			// fmt.Scanln(&input)
+			// input = strings.TrimSpace(input)
+			// input = strings.ToLower(input)
+			// switch input {
+			// case "y", "yes", "ye", "yess":
+			// 	fmt.Printf("Unlinking %s \n", dst)
+			// 	os.Remove(dst)
+			// }
+		}
+	}
 	return true, nil
+}
+
+func backup(dst string) error {
+	bd := filepath.Join(BACKUP, HOSTNAME, dst)
+	if _, err := os.Stat(bd); os.IsNotExist(err) {
+		err := os.Mkdir(bd, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	return nil
 }
 
 func main() {
 	fmt.Printf("Current Hostname: %s\n", HOSTNAME)
 	p := getProfiles()
 	i := askProfileInstall(p)
-	fmt.Println(i)
 	for _, profile := range i {
 		install(profile)
 	}
